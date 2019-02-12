3325bbeb21f03c7e2d9283c308501e32e1cc4478
Konstantin Todorov <kztodorov@outlook.com>
2019-02-12 16:38:37 +0100

    Fix implementation errors with problem 4 from assignment 1

--- assignments/1/color_constancy/AWB.m
+++ assignments/1/color_constancy/AWB.m
@@ -1,42 +1,47 @@
 function [] = AWB(img_name)
 
-// read the image
-I = imread(img_name);
-// get the values for the color channels
-R = I(:,:,1);
-G = I(:,:,2);
-B = I(:,:,3);
+    % read the image
+    I = imread(img_name);
 
-// get means of color channels
-Rm = mean(R(:));
-Gm = mean(G(:));
-Bm = mean(B(:));
+    I = im2double(I);
 
-// generate the corrected grayworld 128 part
-r = 128/Rm;
-g = 128/Gm;
-b = 128/Bm;
+    % get the values for the color channels
+    R = I(:,:,1);
+    G = I(:,:,2);
+    B = I(:,:,3);
 
-// initialize the "corrected" image with zeros
-[w, h, channels] = size(I);
-Ic = zeros(w, h, channels);
+    % get means of color channels
+    Rm = mean(R(:));
+    Gm = mean(G(:));
+    Bm = mean(B(:));
 
-// now we can do the correction
-// by multiplying the values
-// maybe this can be vectorized
-Ic(:,:,1) = I(:,:,1) * r;
-Ic(:,:,2) = I(:,:,2) * g;
-Ic(:,:,3) = I(:,:,3) * b;
+    % generate the corrected grayworld 128 part
+    r = 128/Rm
+    g = 128/Gm
+    b = 128/Bm
 
-// and show the figure
-figure 
-subplot(1,2,1)
-orignal = imshow(I)
-title('Original picture')
-subplot(1,2,2)
-modified = imshow(Ic);
-title('Modified to be color constant')
+    % initialize the "corrected" image with zeros
+    [h, w, channels] = size(I);
+    Ic = zeros(h, w, channels);
 
-end;
+    % now we can do the correction
+    % by multiplying the values
+    % maybe this can be vectorized
+    Ic(:,:,1) = I(:,:,1) * r;
+    Ic(:,:,2) = I(:,:,2) * g;
+    Ic(:,:,3) = I(:,:,3) * b;
+
+    Ic = uint8(Ic);
+
+    % and show the figure
+    figure 
+    subplot(1,2,1)
+    imshow(I)
+    title('Original picture')
+    subplot(1,2,2)
+    imshow(Ic);
+    title('Modified to be color constant')
+
+end
 
 
