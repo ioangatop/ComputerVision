f274b111652c58f22598be9823a837791769fef1
Ioannis Gatopoulos <johngatop@gmail.com>
2019-02-20 18:19:53 +0100

    Addinings

--- assignments/2/1-Image_Enhancement/test_script.m
+++ assignments/2/1-Image_Enhancement/test_script.m
@@ -1,12 +1,16 @@
 clear;
 clc;
+
 %% 3: Low-level filters
+
 % 1D Gaussian Filter
 G1 = gauss1D( 2 , 5 );
 
 % 2D Gaussian Filter
 G2 = gauss2D( 2 , 5 );
+
 %% PSNR
+
 % PSNR on image1 with salt and pepper noise
 I = imread('images/image1.jpg');
 J_sap = imread('images/image1_saltpepper.jpg');
@@ -21,10 +25,10 @@ PSNR_gaussian =  myPSNR( I, J_gauss );
 % Denoise salt and pepper noise
 
 % box
-im = denoise( J_sap, 'box', 3);
+denoised_img_box = denoise( J_sap, 'box', 3);
 
 % median
-im = denoise( J_sap, 'median', 3);
+denoised_img_median = denoise( J_sap, 'median', 3);
 
 % gaussian
-im = denoise( J_sap, 'gaussian', 2, 5);
+denoised_img_gaussian = denoise( J_sap, 'gaussian', 2, 5);
