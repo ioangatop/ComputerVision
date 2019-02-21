555254bfc8f27b134ff8945aa9c2b813bfa2f817
vincent <vincentr@live.nl>
2019-02-21 21:07:40 +0100

    small changes

--- assignments/2/1-Image_Enhancement/compute_LoG.m
+++ assignments/2/1-Image_Enhancement/compute_LoG.m
@@ -21,11 +21,16 @@ switch LOG_type
 
     case 3
         %method 3
+        % ratio s1 / s2
+        K = 5
+        % filter sizes
         H_size = 5;
-        sigma_1 = 1;
-        sigma_2 = sigma_1 * H_size;
+        % sigmas
+        sigma_1 = .5;
+        sigma_2 = sigma_1 * K
         
         % are these equal?
+        % this might be faster - only 1 convolution 
         H_dog = fspecial('gaussian', H_size, sigma_1) - ...
                 fspecial('gaussian', H_size, sigma_2);
         imOut = imfilter(image, H_dog);
@@ -35,7 +40,7 @@ switch LOG_type
         imOut_1 = imfilter(image, H_dog_1);
         H_dog_2 = fspecial('gaussian', H_size, sigma_2);
         imOut_2 = imfilter(image, H_dog_2);
-        imOut = imOut_1 - imOut_2;
+        imOut = imOut_2 - imOut_1;
         %}
       
 end
