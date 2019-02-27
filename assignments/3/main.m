91fd15a89807ba557113d2b6a28a30a9744c5294
Ioannis Gatopoulos <johngatop@gmail.com>
2019-02-27 01:18:31 +0100

    Harris corner ready

--- assignments/3/main.m
+++ assignments/3/main.m
@@ -6,4 +6,5 @@ J = imread('pingpong/0000.jpeg');
 I_45 = imrotate(I, 45);
 I_90 = imrotate(I, 90);
 
-harris_corner_detector(I_45);
+[H, r, c]= harris_corner_detector(J);
+% [H, r, c]= harris_corner_detector(I_90);
