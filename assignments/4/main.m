e7fc00b485dc4358ccda77e026176fca3483a069
Ioannis Gatopoulos <johngatop@gmail.com>
2019-03-04 12:55:45 +0100

    HW 4 - 1.1 and 1.2 done(+)

--- assignments/4/main.m
+++ assignments/4/main.m
@@ -8,4 +8,5 @@ J = imread('boat2.pgm');
 [matches, scores, f_I, f_J] = keypoint_matching(I, J);
 
 % Plot a random subset (size 10) of all matching points
-plot_keypoints_subset(I, J, matches, scores, f_I, f_J, 10);
+sample = 100;
+plot_keypoints_subset(I, J, matches, scores, f_I, f_J, sample);
