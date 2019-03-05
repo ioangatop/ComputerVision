c80ca5a29234ef0b6f88bd413895e2e33e267db2
pollendo <ollendorff.philipp@gmail.com>
2019-03-05 16:43:22 +0100

    update lucas

--- assignments/3/lucas_kanade.m
+++ assignments/3/lucas_kanade.m
@@ -1,27 +1,47 @@
-function [flow] = lucas_kanade()
+function [flow] = lucas_kanade(image1, image2)
 % Lucas Kanade Optical flow 
 
-im1 = double(imread("sphere1.ppm"));
-im2 = double(imread("sphere2.ppm"));
+% Read the two images and compute gradients for entire image
+% im1 = double(imread("synth1.pgm"));
+% im2 = double(imread("synth2.pgm"));
+im1 = double(image1);
+im2 = double(image2);
+[height, width, c] = size(im1);
+
+region_size = 15;
+nr_of_regions = floor(height/region_size)-1;
+
 [gx1, gy1] = gradient(im1);
 [gx2, gy2] = gradient(im2);
 
 final_grid_x = [];
 final_grid_y = [];
-for i = 0:12
+
+% Loop over regions in x direction
+for i = 0:nr_of_regions
     one_row_x = [];
     one_row_y = [];
-    for j = 0:12
-        A_region1 = gx1(i*15+1:(i+1)*15, j*15+1:(j+1)*15).';
-        A_region2 = gy1(i*15+1:(i+1)*15, j*15+1:(j+1)*15).';
+    % Loop over regions in y direction
+    for j = 0:nr_of_regions
+        
+        % Cut image in 15x15 regions of gradients iteratively
+        A_region1 = gx1(i*region_size+1:(i+1)*region_size, j*region_size+1:(j+1)*region_size).';
+        A_region2 = gy1(i*region_size+1:(i+1)*region_size, j*region_size+1:(j+1)*region_size).';
+        
+        % Reshape each region into a 225x2 vector (by stacking each row on
+        % top of each other)
         A = [reshape(A_region1, 225, 1), reshape(A_region2, 225, 1)];
         A_T = A.';
-
+        
+        % Cut regular image in 15x15 regions 
         b_region1 = im1(i*15+1:(i+1)*15, j*15+1:(j+1)*15).';
         b_region2 = im2(i*15+1:(i+1)*15, j*15+1:(j+1)*15).';
-
+        
+        % Reshape each region into a 225x1 vector (by stacking each row on top
+        % of each other)
         b = reshape(b_region1, 225, 1) - reshape(b_region2, 225, 1);
         
+        % Calculate the local image flow vector (v)
         v = mldivide(A_T * A, A_T * b);
         v(isnan(v)) = 0.1;
         one_row_x = [one_row_x, v(1:1)];
@@ -31,12 +51,12 @@ for i = 0:12
     final_grid_y = [final_grid_y; one_row_y];
 end
 
-[x,y] = meshgrid(0:1:11,0:1:12);
-%figure
-quiver(x, y, final_grid_x(:,2:13), final_grid_y(:,2:13)); axis image
-im = imread('sphere1.ppm');
-hax = gca; %get the axis handle
-image(hax.XLim,hax.YLim,im); %plot the image within the axis limits
-hold on; %enable plotting overwrite
-quiver(x, y, final_grid_x(:,2:13), final_grid_y(:,2:13))
-%quiver(x,y,px,py) %plot the quiver on top of the image (same axis limits)
+% Plot on top of original image using quiver
+
+[x,y] = meshgrid(0:1:nr_of_regions,0:1:nr_of_regions);
+quiver(x, y, final_grid_x, final_grid_y); axis image
+im = image1;
+hax = gca; % use initial plot to get the axis
+image(hax.XLim,hax.YLim,im);
+hold on; 
+quiver(x, y, final_grid_x, final_grid_y, "red")
