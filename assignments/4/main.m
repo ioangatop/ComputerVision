a73f25fe89faf2cea31fb8d4b80a5b0b14d678a7
Ioannis Gatopoulos <johngatop@gmail.com>
2019-03-09 13:00:30 +0100

    RANSAC first four steps done

--- assignments/4/main.m
+++ assignments/4/main.m
@@ -7,12 +7,13 @@ run vlfeat-0.9.21/toolbox/vl_setup
 I = imread('boat1.pgm');
 J = imread('boat2.pgm');
 
-% Get the matching points
+%% Get the matching points
 [matches, scores, f_I, f_J, d_I, d_J] = keypoint_matching(I, J);
 
 % Plot a random subset (size = sample) of all matching points
 % sample = 50;
 % plot_keypoints_subset(I, J, matches, scores, f_I, f_J, d_I, d_J, sample);
 
+%% RANSAC
 N = 3; P = 10;
 RANSAC(N, P, I, J)
