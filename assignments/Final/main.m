28ecdb9205192d59a5fb8a0bfa7728f17e7a9bd0
Ioannis Gatopoulos <johngatop@gmail.com>
2019-03-13 16:10:47 +0100

    better comments added

--- assignments/Final/main.m
+++ assignments/Final/main.m
@@ -3,15 +3,13 @@ clear; clc;
 % Command to run vlfeat
 run vlfeat-0.9.21/toolbox/vl_setup;
 
-img = imread('peppers.png');
-
-%% Dense SIFT (e.g. vl dsift) descriptor extraction 
+%% Dense SIFT descriptor extraction (vl dsift)
 type = 'RGB';
 binSize = 8;
 magnif = 3;
 
 img_descriptors = get_densely_sampled_regions(img, type, binSize, magnif);
 
-%% Key points SIFT descriptor extraction (e.g. vl sift)
+%% Key-points SIFT descriptor extraction (vl sift)
 % type = 'RGB';
 % img_descriptors = get_dense(img, type);
