5ff998a6fdc786661659eb3cdab4045a070a6cdf
ktodorov <kztodorov@outlook.com>
2019-03-13 22:06:29 +0100

    Refactored into parse_images function

--- assignments/Final/main.m
+++ assignments/Final/main.m
@@ -6,45 +6,17 @@ run vlfeat-0.9.21/toolbox/vl_setup;
 %% Dense SIFT descriptor extraction (vl dsift)
 
 images = reshape(X, 5000, 96, 96, 3);
-img = reshape(images(1, :, :, :), 96, 96, 3);
-img2 = reshape(images(2, :, :, :), 96, 96, 3);
+[image_descriptors, unused_image_indices] = parse_images(images, y, 5, 100);
 
-
+%% 
 type = 'RGB';
 binSize = 8;
 magnif = 3;
 
-% 100 images per class
-class_count = [0,0,0,0,0];
-img_descriptors1 = get_densely_sampled_regions(img, type, binSize, magnif);
-img_descriptors2 = get_densely_sampled_regions(img2, type, binSize, magnif);
-descriptors = cat(2, img_descriptors1, img_descriptors2);
-
-unused_images = [];
-for i = 3:size(X, 1)
-    current_class = y(i);
-    if current_class > 5
-        continue
-    end
-    
-    if class_count(current_class) < 10
-        current_image = reshape(images(i, :, :, :), 96, 96, 3);
-        class_count(current_class) = class_count(current_class) + 1;
-        current_img_descriptors = get_densely_sampled_regions(current_image, type, binSize, magnif);
-        descriptors = cat(2, descriptors, current_img_descriptors);
-    else
-        unused_images = [unused_images, i];
-    end
-end
-
-%% 
-
 numClusters = 1000;
-descriptors = single(descriptors);
+centers = vl_kmeans(image_descriptors, numClusters);
 
-centers = vl_kmeans(descriptors, numClusters);
-
-img3 = reshape(images(unused_images(1), :, :, :), 96, 96, 3);
+img3 = reshape(images(unused_image_indices(1), :, :, :), 96, 96, 3);
 image_descriptors3 = get_densely_sampled_regions(img3, type, binSize, magnif);
 pdf = calculate_pdf(image_descriptors3, centers);
 
---
+++ assignments/Final/parse_images.m
@@ -0,0 +1,33 @@
+function [image_descriptors, unused_image_indices] = parse_images(image_set, classes_set, classes_count, class_size)
+    type = 'RGB';
+    binSize = 8;
+    magnif = 3;
+
+    img = reshape(image_set(1, :, :, :), 96, 96, 3);
+    img2 = reshape(image_set(2, :, :, :), 96, 96, 3);
+
+    class_count = zeros(1, class_size);
+    img_descriptors1 = get_densely_sampled_regions(img, type, binSize, magnif);
+    img_descriptors2 = get_densely_sampled_regions(img2, type, binSize, magnif);
+    image_descriptors = cat(2, img_descriptors1, img_descriptors2);
+
+    unused_image_indices = [];
+    for i = 3:size(image_set, 1)
+        current_class = classes_set(i);
+        if current_class > classes_count
+            continue
+        end
+
+        if class_count(current_class) < 10
+            current_image = reshape(image_set(i, :, :, :), 96, 96, 3);
+            class_count(current_class) = class_count(current_class) + 1;
+            current_img_descriptors = get_densely_sampled_regions(current_image, type, binSize, magnif);
+            image_descriptors = cat(2, image_descriptors, current_img_descriptors);
+        else
+            unused_image_indices = [unused_image_indices, i];
+        end
+    end
+    
+    image_descriptors = single(image_descriptors);
+end
+
