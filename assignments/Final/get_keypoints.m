f2a538592618b52914d686d90e316905b8b72c1b
Ioannis Gatopoulos <johngatop@gmail.com>
2019-03-13 16:32:52 +0100

    opp color space added

--- assignments/Final/get_keypoints.m
+++ assignments/Final/get_keypoints.m
@@ -10,13 +10,21 @@ function descriptors = get_keypoints(img, type)
 
 if channels == 3
     img = single(img);
-    if type == 'RGB'
-        [~, descriptor_1] = vl_sift(img(:,:,1));
-        [~, descriptor_2] = vl_sift(img(:,:,2));
-        [~, descriptor_3] = vl_sift(img(:,:,3));
-        descriptors = cat(2, descriptor_1, descriptor_2, descriptor_3);
-    elseif  type == 'OPP'
+    if type == 'OPP'
+        channel_1 = img(:,:,1);
+        channel_2 = img(:,:,2);
+        channel_3 = img(:,:,3);
         
+        % O1, O2 and O3 correspont to the oppoment color space channels
+        % Ok = img(:,:,k), where k = {1, 2, 3}
+        img(:,:,1) = single((channel_1-channel_2)/sqrt(2));
+        img(:,:,2) = single((channel_1+channel_2-(2*channel_3))/sqrt(6));
+        img(:,:,3) = single(((channel_1+channel_2+channel_3)/sqrt(3)) ;
+        
+    [~, descriptor_1] = vl_sift(img(:,:,1));
+    [~, descriptor_2] = vl_sift(img(:,:,2));
+    [~, descriptor_3] = vl_sift(img(:,:,3));
+    descriptors = cat(2, descriptor_1, descriptor_2, descriptor_3);
     end
 else % if image is greyscale
     [~, descriptors] = vl_sift(img);
