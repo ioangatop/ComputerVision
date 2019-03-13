ae831d4d5223b084c853196282bd195873d48a8c
pollendo <ollendorff.philipp@gmail.com>
2019-03-13 16:55:13 +0100

    add for loop to dynamically cluster images

--- assignments/Final/main.m
+++ assignments/Final/main.m
@@ -14,19 +14,34 @@ type = 'RGB';
 binSize = 8;
 magnif = 3;
 
+% 100 images per class
+class_count = [0,0,0,0,0];
 img_descriptors1 = get_densely_sampled_regions(img, type, binSize, magnif);
 img_descriptors2 = get_densely_sampled_regions(img2, type, binSize, magnif);
-
 descriptors = cat(2, img_descriptors1, img_descriptors2);
+
+unused_images = [];
+for i = 3:size(X, 1)
+    current_image = reshape(images(i, :, :, :), 96, 96, 3);
+    current_class = y(i);
+    if class_count(current_class) < 100
+        class_count(current_class) = class_count(current_class) + 1;
+        current_img_descriptors = get_densely_sampled_regions(current_image, type, binSize, magnif);
+        descriptors = cat(2, descriptors, current_img_descriptors);
+    else
+        unused_images = [unused_images, current_image];
+    end
+end
+
 
 %% Key-points SIFT descriptor extraction (vl sift)
 
-numClusters = 10;
+numClusters = 1000;
 descriptors = single(descriptors);
 
 centers = vl_kmeans(descriptors, numClusters);
 
-image_descriptors3 = get_densely_sampled_regions(img3, type, binSize, magnif);
+image_descriptors3 = get_densely_sampled_regions(unused_images(1), type, binSize, magnif);
 probability_distribution = calculate_probability_distribution(image_descriptors3, centers, type, binSize, magnif);
 
 histogram(probability_distribution, numClusters);
