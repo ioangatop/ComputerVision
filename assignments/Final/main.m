8f912df952532d70bdfc65f46d966c2c6c8678ab
ktodorov <kztodorov@outlook.com>
2019-03-15 18:38:15 +0100

    Added used image indices matrix

--- assignments/Final/main.m
+++ assignments/Final/main.m
@@ -5,13 +5,32 @@ run vlfeat-0.9.21/toolbox/vl_setup;
 
 %% Dense SIFT descriptor extraction (vl dsift)
 
-images = reshape(X, 5000, 96, 96, 3);
-[image_descriptors, unused_image_indices] = parse_images(images, y, 5, 20);
+height = 96;
+width = 96;
+channels = 3;
+
+images = reshape(X, 5000, height, width, channels);
+classes_count = 5;
+class_size = 20;
+[image_descriptors, used_image_indices, unused_image_indices] = parse_images(images, y, classes_count, class_size);
 
 %% Cluster images and build visual vocabulary and dictionary
 clusters_amount = 1000;
 centers = vl_kmeans(image_descriptors, clusters_amount);
 pdfs = calculate_pdfs(images([unused_image_indices(1) unused_image_indices(2)], :, :, :), centers); 
+
+used_images_per_class = zeros(classes_count, class_size);
+
+used_y = y(used_image_indices);
+for i = 1:classes_count
+    y_class = find(used_y == i);
+    used_images_per_class(i, :) = y_class;
+end
+
+%%
+
+
+
 
 % visual_dictionary = create_visual_dictionary(centers, images(unused_image_indices, :, :, :));
 
--- assignments/Final/parse_images.m
+++ assignments/Final/parse_images.m
@@ -1,4 +1,4 @@
-function [image_descriptors, unused_image_indices] = parse_images(image_set, classes_set, classes_count, class_size)
+function [image_descriptors, used_image_indices, unused_image_indices] = parse_images(image_set, classes_set, classes_count, class_size)
     type = 'RGB';
     binSize = 8;
     magnif = 3;
@@ -13,15 +13,18 @@ function [image_descriptors, unused_image_indices] = parse_images(image_set, cla
     image_descriptors = cat(2, img_descriptors1, img_descriptors2);
 
     unused_image_indices = [];
+    used_image_indices = [];
     for i = 3:size(image_set, 1)
         current_class = classes_set(i);
         if current_class > classes_count
             continue
-        elseif class_count(current_class) < 10
+            
+        elseif class_count(current_class) < class_size
             current_image = reshape(image_set(i, :, :, :), 96, 96, 3);
             class_count(current_class) = class_count(current_class) + 1;
             current_img_descriptors = get_densely_sampled_regions(current_image, type, binSize, magnif, Step);
             image_descriptors = cat(2, image_descriptors, current_img_descriptors);
+            used_image_indices = [used_image_indices, i];
         else
             unused_image_indices = [unused_image_indices, i];
         end
